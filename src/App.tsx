e6456ba946d121d77ac65d1fd0eda030f5e6dcc8
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-05-13 12:36:00 +0200

    tailwind er sat op igen

--- src/App.tsx
+++ src/App.tsx
@@ -16,20 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
 export default App
