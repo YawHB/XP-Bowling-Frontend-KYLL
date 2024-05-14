8bde21f43bdd575dbc0076c3a5b733a13b283709
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-05-14 11:26:35 +0200

    adjusted css to fit screen width

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -9,45 +9,20 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps): JSX.Element {
   return (
-    <div className="flex flex-col h-screen w-screen">
+    <div className="flex flex-col h-screen">
       <header className="text-white">
         <div className="container mx-auto">
-          {" "}
-          {/* Center the content and add padding */}
           <NavHeader />
         </div>
       </header>
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow">
+        <div className="container mx-auto">{children}</div>
+      </main>
       <footer className="text-white">
         <div className="container mx-auto">
-          {" "}
-          {/* Center the content and add padding */}
           <Footer />
         </div>
       </footer>
     </div>
   );
 }
-
-// import { ReactNode } from "react";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-
-// // Define Props type
-// type LayoutProps = {
-//   children: ReactNode;
-// };
-
-// export default function Layout({ children }: LayoutProps): JSX.Element {
-//   return (
-//     <div className="flex flex-col h-screen justify-between">
-//       <header className="" >
-//         <Navbar />
-//       </header>
-//       {children}
-//       <footer className="">
-//         <Footer />
-//       </footer>
-//     </div>
-//   );
-// }
