7608672612d23531a49492b064fafd01b3c57fea
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-05-15 14:41:09 +0200

    select started

--- src/components/booking/Onlinebooking.tsx
+++ src/components/booking/Onlinebooking.tsx
@@ -1,10 +1,14 @@
 import BookingOverview from "./BookingOverview";
+import BookingSelectActivity from "./BookingSelectActivity";
 
 export default function Booking() {
    return (
      <div className="flex w-screen">
        <div className=" mx-auto px-4 py-8">
          <h1 className="text-4xl self-center font-bold text-red-500">Book en aktivitet her!!</h1>
+       </div>
+       <div>
+          <BookingSelectActivity />
        </div>
        <div>
         <BookingOverview />
