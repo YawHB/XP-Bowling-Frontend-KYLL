99e913e8cb2f1f0617d5bdb657e28cef19c65d6a
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-05-17 16:49:27 +0200

    hh

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./components/standardLayout/Home";
 import BowlingInfo from "./components/infoPages/BowlingInfo";
 import AirhockeyInfo from "./components/infoPages/AirhockeyInfo";
 import RestaurantInfo from "./components/infoPages/RestaurantInfo";
-import Booking from "./components/booking/OnlineBooking";
+import OnlineBooking from "./components/booking/OnlineBooking";
 import BarInfo from "./components/infoPages/BarInfo";
 import BarSale from "./components/sale/BarSale";
 
@@ -16,7 +16,7 @@ export default function App() {
         <Route path="/bowlingInfo" element={<BowlingInfo />} />
         <Route path="/airHockeyInfo" element={<AirhockeyInfo />} />
         <Route path="/restaurant" element={<RestaurantInfo />} />
-        <Route path="/booking" element={<Booking />} />
+        <Route path="/booking" element={<OnlineBooking />} />
         {/* <Route path="about" element={<About />} />
       <Route path="services" element={<Services />} />
       <Route path="contact" element={<Contact />} /> */}
