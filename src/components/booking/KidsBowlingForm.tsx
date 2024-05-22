7164f546e9c4ab153808dc21aec2007cab560d3d
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-05-22 22:48:28 +0200

    ActivityParticipantsDataExtracted

--- src/components/booking/KidsBowlingForm.tsx
+++ src/components/booking/KidsBowlingForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 interface KidsBowlingFormProps {
   addBooking: (newBooking: BookingData) => void;
@@ -13,20 +13,38 @@ interface BookingData {
   lanes: number;
 }
 
+interface LaneInput {
+  laneNumber: number;
+  textInputValues: string[];
+}
+
 export default function KidsBowlingForm({ addBooking }: KidsBowlingFormProps) {
-  const [startTime, setStartTime] = useState<string>("08:00");  
-    const [endTime, setEndTime] = useState<string>(
-      calculatedEndTime("08:00", 1)
-    );
-  const [playTime, setPlayTime] = useState<number>(0);
+  const [startTime, setStartTime] = useState<string>("08:00");
+  const [playTime, setPlayTime] = useState<number>(1);
+  const [endTime, setEndTime] = useState<string>(calculatedEndTime("08:00", 1));
   const [lanes, setLanes] = useState<number>(1);
+  const [laneInputs, setLaneInputs] = useState<LaneInput[]>(
+    Array.from({ length: lanes }, (_, laneIndex) => ({
+      laneNumber: laneIndex + 1,
+      textInputValues: Array(6).fill(""), // Assuming 6 inputs per lane
+    }))
+  );
   const [addNames, setAddNames] = useState<boolean>(false);
 
+  useEffect(() => {
+    setLaneInputs(
+      Array.from({ length: lanes }, (_, laneIndex) => ({
+        laneNumber: laneIndex + 1,
+        textInputValues: Array(6).fill(""), // Assuming 6 inputs per lane
+      }))
+    );
+  }, [lanes]);
+
   function calculatedEndTime(startTime: string, playTime: number): string {
-    const [hours, minuttes] = startTime.split(":").map(Number);
+    const [hours, minutes] = startTime.split(":").map(Number);
     const endTime = new Date();
     endTime.setHours(hours + playTime);
-    endTime.setMinutes(minuttes);
+    endTime.setMinutes(minutes);
     return endTime.toTimeString().slice(0, 5);
   }
 
@@ -62,17 +80,54 @@ export default function KidsBowlingForm({ addBooking }: KidsBowlingFormProps) {
     setAddNames(event.target.checked);
   }
 
+  const handleInputChange = (
+    laneIndex: number,
+    inputIndex: number,
+    value: string
+  ) => {
+    const updatedLaneInputs = laneInputs.map((lane, lIndex) => {
+      if (lIndex === laneIndex) {
+        const updatedTextInputValues = lane.textInputValues.map(
+          (input, iIndex) => {
+            if (iIndex === inputIndex) {
+              return value;
+            }
+            return input;
+          }
+        );
+        return { ...lane, textInputValues: updatedTextInputValues };
+      }
+      return lane;
+    });
+
+    setLaneInputs(updatedLaneInputs);
+  };
+
+  console.log("This is laneInputs:");
+  console.log(laneInputs);
+
   return (
-    <form onSubmit={handleSubmit} className="container mx-auto px-4 py-8 max-w-screen-md">
+    <form
+      onSubmit={handleSubmit}
+      className="container mx-auto px-4 py-8 max-w-screen-md"
+    >
       <section className="flex flex-wrap -mx-2">
         <div className="w-full px-2 mb-4">
-          <h2 className="text-2xl font-bold">Kids Bowling</h2>
+          <h2 className="text-2xl font-bold">Bowling</h2>
         </div>
         <div className="w-full sm:w-1/4 px-2 mb-4 sm:mb-0">
-          <label htmlFor="laneSelector" className="block mb-2 text-sm font-medium">
+          <label
+            htmlFor="laneSelector"
+            className="block mb-2 text-sm font-medium"
+          >
             Baner
           </label>
-          <select id="laneSelector" onChange={handleLaneSelectChange} value={lanes} className="bg-black text-white block w-full p-2 border border-gray-300 rounded-md">
+          <select
+            id="laneSelector"
+            onChange={handleLaneSelectChange}
+            value={lanes}
+            className="block w-full p-2 border border-gray-300 rounded-md bg-black text-white"
+          >
             <option value={1}>1</option>
             <option value={2}>2</option>
             <option value={3}>3</option>
@@ -81,20 +136,36 @@ export default function KidsBowlingForm({ addBooking }: KidsBowlingFormProps) {
         </div>
 
         <div className="w-full sm:w-1/4 px-2 mb-4 sm:mb-0">
-          <label htmlFor="playTimeSelector" className="block mb-2 text-sm font-medium">
+          <label
+            htmlFor="playTimeSelector"
+            className="block mb-2 text-sm font-medium"
+          >
             Timer
           </label>
-          <select id="playTimeSelector" onChange={handlePlayTimeChange} value={playTime} className="block w-full p-2 border border-gray-300 rounded-md bg-black text-white">
+          <select
+            id="playTimeSelector"
+            onChange={handlePlayTimeChange}
+            value={playTime}
+            className="bg-black text-white block w-full p-2 border border-gray-300 rounded-md"
+          >
             <option value={1}>1</option>
             <option value={2}>2</option>
           </select>
         </div>
 
-        <div title="selectStartTime" className="w-full sm:w-1/4 px-2 mb-4 sm:mb-0">
+        <div
+          title="selectStartTime"
+          className="w-full sm:w-1/4 px-2 mb-4 sm:mb-0"
+        >
           <label htmlFor="startTime" className="block mb-2 text-sm font-medium">
             Start Tid
           </label>
-          <select id="startTime" onChange={handleStartTimeChange} value={startTime} className="bg-black text-white block w-full p-2 border border-gray-300 rounded-md">
+          <select
+            id="startTime"
+            onChange={handleStartTimeChange}
+            value={startTime}
+            className="bg-black text-white block w-full p-2 border border-gray-300 rounded-md"
+          >
             <option value={"08:00"}>08:00</option>
             <option value={"09:00"}>09:00</option>
             <option value={"10:00"}>10:00</option>
@@ -113,20 +184,28 @@ export default function KidsBowlingForm({ addBooking }: KidsBowlingFormProps) {
 
         <div className="w-full sm:w-1/4 px-2 mb-4 sm:mb-0">
           <label className="block mb-2 text-sm font-medium">Tilføj navne</label>
-          <input type="checkbox" onChange={handleCheckboxChange} checked={addNames} />
+          <input type="checkbox" onChange={handleCheckboxChange} />
         </div>
       </section>
 
       {addNames && (
         <div className="mt-4">
-          {Array.from({ length: lanes }, (_, laneIndex) => (
+          {laneInputs.map((lane, laneIndex) => (
             <section key={laneIndex} className="flex flex-wrap -mx-2 mt-4">
-              {[...Array(6)].map((_, index) => (
+              {lane.textInputValues.map((inputValue, index) => (
                 <div key={index} className="w-full sm:w-1/2 px-2 mb-4">
                   <label className="block mb-2 text-sm font-medium">
-                    Navn {index + 1} (Bane {laneIndex + 1})
+                    Navn {index + 1} (Bane {lane.laneNumber})
                   </label>
-                  <input type="text" className="block w-full p-2 border border-gray-300 rounded-md text-black" placeholder={`Enter name ${index + 1}`} />
+                  <input
+                    type="text"
+                    className="block w-full p-2 border border-gray-300 rounded-md text-black"
+                    placeholder={`Enter name ${index + 1}`}
+                    value={inputValue}
+                    onChange={(e) =>
+                      handleInputChange(laneIndex, index, e.target.value)
+                    }
+                  />
                 </div>
               ))}
             </section>
@@ -134,7 +213,10 @@ export default function KidsBowlingForm({ addBooking }: KidsBowlingFormProps) {
         </div>
       )}
 
-      <button type="submit" className="w-full sm:w-1/4 p-2 mt-4 bg-green-500 text-white rounded-md">
+      <button
+        type="submit"
+        className="w-full sm:w-1/4 p-2 mt-4 bg-green-500 text-white rounded-md"
+      >
         Tilføj
       </button>
     </form>
