d14552dbb7874fb827f67ee583315bcc6e87f435
YawHB <yawhboateng@gmail.com>
2024-05-23 22:45:15 +0200

    created fetch shifts. OBS change endpoint from sales to shifts. i didnt have the shift controller available to use

---
+++ src/api/shift/getAllShiftsApi.ts
@@ -0,0 +1,22 @@
+import { Shift } from '../../components/shift/ShiftOverviews';
+
+export default async function getAllShiftsApi() {
+    try {
+        const getShifts = async () => {
+            const response = await fetch('http://localhost:8080/sales');
+
+            if (!response.ok) {
+                const errorMessage = await response.text();
+                console.log('Errormessage :O: ' + errorMessage);
+                return;
+            }
+            const shifts: Shift[] = await response.json();
+            console.log(shifts);
+            return shifts;
+        };
+
+        getShifts();
+    } catch (error) {
+        console.log('Error ' + error);
+    }
+}
--- src/components/shift/ShiftOverviews.tsx
+++ src/components/shift/ShiftOverviews.tsx
@@ -1,44 +1,50 @@
-import { useState } from "react";
-import ShiftTable from "./ShiftTable";
-
-interface Shift {
-  id: number;
-  date: string;
-  startDate: string;
-  placeName: string;
-  employee: {
+import { useEffect, useState } from 'react';
+import ShiftTable from './ShiftTable';
+export interface Shift {
     id: number;
-    firstName: string;
-    lastName: string;
-    employeeRole: {
-      id: number;
-      employeeRole: string;
+    date: string;
+    startDate: string;
+    placeName: string;
+    employee: {
+        id: number;
+        firstName: string;
+        lastName: string;
+        employeeRole: {
+            id: number;
+            employeeRole: string;
+        };
     };
-  };
 }
 
 export default function ShiftOverview() {
-  //useState for shifts
-  const [shifts, setShifts] = useState<Shift[]>([]);
+    const [shifts, setShifts] = useState<Shift[]>([]);
 
+    useEffect(() => {
+        try {
+            const getShifts = async () => {
+                const response = await fetch('http://localhost:8080/sales');
+                const data = await response.json();
+                console.log(data);
+                setShifts(data);
+            };
+            console.log(shifts);
+            getShifts();
+        } catch (error) {
+            console.log('Error ' + error);
+        }
+    }, []);
 
-
-  //Use effect
-
-
-  
-
-  return (
-    <div className="w-screen px-4">
-      <h1 className="mb-4">ShiftOverview</h1>
-      <h3 className="mb.4 text-white">Vælg dato: Kalender</h3>
-      {/* Send shifts som prop i Shifts component */}
-      <ShiftTable />
-      <h3 className="text-white mb.4">
-        Tilføj vagt: Role-dropdown medarbejder-dropdown
-        {/* Form med  2 dropdowns */}
-      </h3>
-      <button>Tilføj vagt</button>
-    </div>
-  );
+    return (
+        <div className="w-screen px-4">
+            <h1 className="mb-4">ShiftOverview</h1>
+            <h3 className="mb.4 text-white">Vælg dato: Kalender</h3>
+            {/* Send shifts som prop i Shifts component */}
+            <ShiftTable />
+            <h3 className="text-white mb.4">
+                Tilføj vagt: Role-dropdown medarbejder-dropdown
+                {/* Form med  2 dropdowns */}
+            </h3>
+            <button>Tilføj vagt</button>
+        </div>
+    );
 }
