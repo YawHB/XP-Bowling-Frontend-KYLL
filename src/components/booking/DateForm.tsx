5d81cac8474be18a634a61bf5db2a9c51f0d0031
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-05-26 18:25:25 +0200

    alternativ date chooser

--- src/components/booking/DateForm.tsx
+++ src/components/booking/DateForm.tsx
@@ -1,5 +1,5 @@
-import DatePicker from 'react-date-picker';
-import './DateForm.css';
+import DatePicker from "react-date-picker";
+import "./DateForm.css";
 //import { Value } from "./DateForm";
 //import { OnlineBookingProps } from "./OnlineBooking";
 
@@ -7,34 +7,42 @@ type ValuePiece = Date | null;
 export type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function DateForm({ bookingDate, setBookingDate, setFormattedDate }: { bookingDate: Value; setBookingDate: (value: Value) => void; setFormattedDate: (date: Date | null) => void }) {
-    function checkDate(event: React.FormEvent<HTMLButtonElement>) {
-        event.preventDefault();
+  function checkDate(event: React.FormEvent<HTMLButtonElement>) {
+    event.preventDefault();
 
-        const date = convertToDate(bookingDate);
-        setFormattedDate(date);
+    const date = convertToDate(bookingDate);
+    setFormattedDate(date);
 
-        if (date) {
-            console.log(date.toISOString()); // or any other format you need
-        } else {
-            console.log('No valid date selected');
-        }
+    if (date) {
+      console.log(date.toISOString()); // or any other format you need
+    } else {
+      console.log("No valid date selected");
     }
+  }
 
-    function convertToDate(value: Value): Date | null {
-        if (value instanceof Date) {
-            return value;
-        } else if (Array.isArray(value)) {
-            return value[0] instanceof Date ? value[0] : null;
-        }
-        return null;
+  function convertToDate(value: Value): Date | null {
+    if (value instanceof Date) {
+      return value;
+    } else if (Array.isArray(value)) {
+      return value[0] instanceof Date ? value[0] : null;
     }
+    return null;
+  }
 
-    return (
-        <div>
-            <DatePicker onChange={setBookingDate} value={bookingDate} />
-            <button type="submit" className="bg-black" onClick={checkDate}>
-                Bekræft Dato
-            </button>
-        </div>
-    );
+  return (
+    //   <div>
+    //     {/* <DatePicker onChange={setBookingDate} value={bookingDate} /> */}
+    //     <input type="date" onChange={(e) => setBookingDate(e.target.value)} value={bookingDate} />
+    //     <button type="submit" className="bg-black" onClick={checkDate}>
+    //       Bekræft Dato
+    //     </button>
+    //   </div>
+
+    <div>
+      <DatePicker onChange={setBookingDate} value={bookingDate} />
+      <button type="submit" className="bg-black" onClick={checkDate}>
+        Bekræft Dato
+      </button>
+    </div>
+  );
 }
