26d522744943827832b04a8e0288cf277f24fdce
YawHB <yawhboateng@gmail.com>
2024-05-26 15:28:02 +0200

    reinstalled dependencies due to run time issues

--- src/components/booking/DateForm.tsx
+++ src/components/booking/DateForm.tsx
@@ -1,23 +1,24 @@
 // import { useState } from "react";
-import DatePicker from "react-date-picker";
-import "./DateForm.css";
+import DatePicker from 'react-date-picker';
+import './DateForm.css';
 
 type ValuePiece = Date | null;
 export type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-export default function DateForm({bookingDate, setBookingDate}: {bookingDate: Value, setBookingDate: (value: Value) => void})  {
-  // const [bookingDate, setBookingDate] = useState<Value>(new Date());
+export default function DateForm({ bookingDate, setBookingDate }: { bookingDate: Value; setBookingDate: (value: Value) => void }) {
+    // const [bookingDate, setBookingDate] = useState<Value>(new Date());
 
-  function checkDate(event: React.FormEvent<HTMLButtonElement>) {
-    event.preventDefault();
-    console.log(bookingDate);
-  }
+    function checkDate(event: React.FormEvent<HTMLButtonElement>) {
+        event.preventDefault();
+        console.log(bookingDate);
+    }
 
-
-  return (
-    <div>
-      <DatePicker onChange={setBookingDate} value={bookingDate} />
-      <button type="submit" onClick={checkDate}>Bekræft Dato</button>
-    </div>
-  );
+    return (
+        <div>
+            <DatePicker onChange={setBookingDate} value={bookingDate} />
+            <button type="submit" onClick={checkDate}>
+                Bekræft Dato
+            </button>
+        </div>
+    );
 }
