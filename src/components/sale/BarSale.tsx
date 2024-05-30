bf877e44c3476bff9fdfbff613a825f944b76810
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-05-30 23:37:23 +0200

    hihi gg

--- src/components/sale/BarSale.tsx
+++ src/components/sale/BarSale.tsx
@@ -68,30 +68,34 @@ export default function BarSale() {
         <div className="w-full md:w-1/3 p-4">
           <div className="container mx-auto px-4 py-8">
             <h1 className="w-full text-3xl font-bold md:w-1/2 text-pink-300 mb-4">Barsalg</h1>
-            <table className="table-auto bg-blue-400">
+            <table className="table-auto bg-gray-600/50 border-2 w-full">
               <thead>
-                <tr>
+                <tr className="border-2">
                   <th className="px-4 py-2">Vare</th>
-                  <th className="px-4 py-2">Pris</th>
                   <th className="px-4 py-2">Antal</th>
+                  <th className="px-4 py-2">Pris</th>
                   <th className="px-4 py-2"></th>
                 </tr>
               </thead>
               <tbody>
                 {consumables.map((consumable) => (
                   <tr key={consumable.id}>
-                    <td>{consumable.title}</td>
-                    <td>{consumable.price}</td>
-                    <td>{consumable.amount},00 kr</td>
-                    <td>
-                      <button onClick={() => handleAddOne(consumable.id)}>+</button>
-                      <button onClick={() => handleSubtractOne(consumable.id)}>-</button>
+                    <td className=" px-4 py-2">{consumable.title}</td>
+                    <td className=" px-4 py-2">{consumable.amount}</td>
+                    <td className=" px-4 py-2">{consumable.price},00 kr</td>
+                    <td className=" px-4 py-2">
+                      <button className="w-6  bg-black text-white" onClick={() => handleAddOne(consumable.id)}>
+                        +
+                      </button>
+                      <button className="w-6 mr-2 bg-black text-white" onClick={() => handleSubtractOne(consumable.id)}>
+                        -
+                      </button>
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" type="submit" onClick={handleCheckout}>
+            <button className="mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" type="submit" onClick={handleCheckout}>
               Bekræft
             </button>
             <CheckoutSummary items={checkoutItems} />
@@ -116,4 +120,62 @@ export default function BarSale() {
         </div>
       </div>
     );
+
+    // return (
+    //   <div className="w-screen flex flex-wrap flex-row">
+    //     <div className="w-full md:w-1/3 p-4">
+    //       <div className="container mx-auto px-4 py-8">
+    //         <h1 className="w-full text-3xl font-bold md:w-1/2 text-pink-300 mb-4 ">Barsalg</h1>
+    //         <table className=" table-auto bg-gray-600/50 border-2 p-4 ">
+    //           <thead>
+    //             <tr className="border-2">
+    //               <th className=" px-4 py-2">Vare</th>
+    //               <th className=" px-4 py-2">Antal</th>
+    //               <th className=" px-4 py-2">Pris</th>
+    //               <th className=" px-4 py-2"></th>
+    //             </tr>
+    //           </thead>
+    //           <tbody>
+    //             {consumables.map((consumable) => (
+    //               <tr key={consumable.id}>
+    //                 <td>{consumable.title}</td>
+    //                 <td>{consumable.amount}</td>
+    //                 <td>{consumable.price},00 kr</td>
+    //                 <td>
+    //                   <button className="w-6 mx-2 bg-black text-white" onClick={() => handleAddOne(consumable.id)}>
+    //                     +
+    //                   </button>
+    //                   <button className="w-6 mr-2 bg-black text-white" onClick={() => handleSubtractOne(consumable.id)}>
+    //                     -
+    //                   </button>
+    //                 </td>
+    //               </tr>
+    //             ))}
+    //           </tbody>
+    //         </table>
+    //         <button className="mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" type="submit" onClick={handleCheckout}>
+    //           Bekræft
+    //         </button>
+    //         <CheckoutSummary items={checkoutItems} />
+    //       </div>
+    //     </div>
+    //     <div className="w-full md:w-1/3 p-4">
+    //       <div className="flex flex-col items-start px-4 py-8">
+    //         <CreateProduct
+    //           onProductCreated={() => {
+    //             getAllConsumables().then((data: Consumable[] | undefined) => {
+    //               const consumablesWithQuantity = (data || []).map((consumable: Consumable) => ({ ...consumable, amount: 0 }));
+    //               setConsumables(consumablesWithQuantity);
+    //             });
+    //           }}
+    //         />
+    //       </div>
+    //     </div>
+    //     <div className="w-full md:w-1/3 p-4">
+    //       <div className="flex flex-col items-start px-4 py-8">
+    //         <UpdateProductPrice consumables={consumables} setConsumablesInBarSale={setConsumables} />
+    //       </div>
+    //     </div>
+    //   </div>
+    // );
 }
--- src/components/sale/CheckOutSummary.tsx
+++ src/components/sale/CheckOutSummary.tsx
@@ -5,15 +5,17 @@ interface CheckoutSummaryProps {
 export const CheckoutSummary = ({ items }: CheckoutSummaryProps) => {
     const totalPrice = items.reduce((acc, item) => acc + item.amount * item.price, 0);
     return (
-        <div>
-            <h2>Valgte varer:</h2>
-            {items.map((item) => (
-                <p key={item.id}>
-                    {item.title}: {item.amount} stk
-                </p>
-            ))}
-            -------------------------
-            {items.length > 0 && <h3 className="h-3">Totalpris: {totalPrice},00 kr</h3>}
-        </div>
+      <div className="my-4 p-4 border-2">
+        <h2 className="font-bold">Valgte varer:</h2>
+        {items.map((item) => (
+          <p key={item.id}>
+            {item.title}: {item.amount} stk
+          </p>
+        ))}
+        <div className="my-4"></div>
+        <hr />
+        <div className="my-4"></div>
+        {items.length > 0 && <h3 className="h-3  font-bold">Totalpris: {totalPrice},00 kr</h3>}
+      </div>
     );
 };
